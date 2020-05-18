f77f13318be0c315b20df9c5e436403ed0c6acb1
Lucille Sherman <lsherman@gatehousemedia.com>
2020-05-18 09:35:10 -0400

    5/15 numbers update

--- outbreaks.R
+++ outbreaks.R
@@ -2,15 +2,18 @@ library(tidyverse)
 library(janitor)
 library(here)
 
+### SIXTH DHHS REPORT
+may15outbreaks <- read_csv(here("outbreaks", "05-15-outbreaks.csv"),  col_names = c("facility_type", "facility_county", "facility", "staff_cases", "staff_deaths", "resident_cases", "resident_deaths", "total_cases", "total_deaths")) %>% 
+  mutate(releasedate = "5/15") %>% 
+  filter(!is.na(total_cases))
+
 ### FIFTH DHHS REPORT
-# cleaned facilities in text editor before importing
 may12outbreaks <- read_csv(here("outbreaks", "05-12-outbreak.csv"), col_names = c("facility_type", "facility_county", "facility", "staff_cases", "staff_deaths", "resident_cases", "resident_deaths", "total_cases", "total_deaths")) %>% 
   mutate(facility = str_replace(facility, "Center\\)", "Genesis Healthcare LLC (Siler City Center)")) %>% 
   mutate(releasedate = "5/12") %>% 
   filter(!is.na(total_cases))
 
 ### FOURTH DHHS REPORT
-# cleaned facilities in text editor before importing
 may8outbreaks <- read_csv(here("outbreaks", "05-08-outbreak.csv"), col_names = c("facility_type", "facility_county", "facility", "staff_cases", "staff_deaths", "resident_cases", "resident_deaths", "total_cases", "total_deaths")) %>% 
   mutate(facility = str_replace(facility, "Center\\)", "Genesis Healthcare LLC (Siler City Center)")) %>% 
   mutate(releasedate = "5/8") %>% 
@@ -59,45 +62,45 @@ april27outbreaks <- bind_cols(april27cases, april27facilities)  %>%
   mutate(facility = str_replace(facility, "Autumn Care - Marshville", "Autumn Care- Marshville"))
 
 # combine
-alloutbreaks <- bind_rows(april27outbreaks, may1outbreaks, may5outbreaks, may8outbreaks, may12outbreaks)
+alloutbreaks <- bind_rows(april27outbreaks, may1outbreaks, may5outbreaks, may8outbreaks, may12outbreaks, may15outbreaks)
 
 ### ANALYZE
 # total cases and deaths for all facilities in the latest report
-may12outbreaks %>% 
+may15outbreaks %>% 
   adorn_totals(where = "row") %>% tail(5)
 
 # total cases and deaths for just nursing homes
-may12outbreaks %>% filter(facility_type == "Nursing Home") %>% 
+may15outbreaks %>% filter(facility_type == "Nursing Home") %>% 
   adorn_totals(where = "row") %>% tail(5)
 
 # differences between latest two reports
 alloutbreaks %>% 
   select(facility, releasedate, total_cases)%>% 
   spread(releasedate, total_cases) %>% 
-  mutate(diff = `5/12` - `5/8`) %>% View()
-mutate(cases512 = as.numeric(ifelse(releasedate == "5/12", total_cases, NA))) %>% 
-  mutate(cases58 = as.numeric(ifelse(releasedate == "5/8", total_cases, NA))) %>% 
-  filter(releasedate == "5/12" | releasedate == "5/8") %>% 
-  mutate(diff = sum(cases512 - cases58)) %>% View()
+  mutate(diff = `5/15` - `5/12`) %>% View()
+  # mutate(cases512 = as.numeric(ifelse(releasedate == "5/15", total_cases, NA))) %>% 
+  # mutate(cases58 = as.numeric(ifelse(releasedate == "5/12", total_cases, NA))) %>% 
+  # filter(releasedate == "5/15" | releasedate == "5/12") %>% 
+  # mutate(diff = sum(cases512 - cases58)) %>% View()
 
 # differences between latest report and one week prior
 alloutbreaks %>% 
   select(facility, releasedate, total_cases)%>% 
   spread(releasedate, total_cases) %>% 
-  mutate(diff = `5/12` - `5/8`) %>% View()
-mutate(cases512 = as.numeric(ifelse(releasedate == "5/12", total_cases, NA))) %>% 
-  mutate(cases58 = as.numeric(ifelse(releasedate == "5/5", total_cases, NA))) %>% 
-  filter(releasedate == "5/12" | releasedate == "5/5") %>% 
-  mutate(diff = sum(cases512 - cases58)) %>% View()
+  mutate(diff = `5/15` - `5/8`) %>% View()
+# mutate(cases512 = as.numeric(ifelse(releasedate == "5/12", total_cases, NA))) %>% 
+#   mutate(cases58 = as.numeric(ifelse(releasedate == "5/5", total_cases, NA))) %>% 
+#   filter(releasedate == "5/12" | releasedate == "5/5") %>% 
+#   mutate(diff = sum(cases512 - cases58)) %>% View()
 
 # list of the facility names for two latest reports
-facilities58 <- may8outbreaks$facility
-
 facilities512 <- may12outbreaks$facility
 
+facilities515 <- may15outbreaks$facility
+
 # which facilities are new additions to the latest report
-may12outbreaks %>% 
-  filter(!facilities512 %in% may8outbreaks$facility) %>% View()
+may15outbreaks %>% 
+  filter(!facilities515 %in% may12outbreaks$facility) %>% View()
 
 # finding facilities that have been removed
 # how many times do facilities show up? 
@@ -131,9 +134,9 @@ allfacilities <- alloutbreaks$facility
 
 # df of just the latest outbreaks
 lasttwo <- alloutbreaks %>% 
-  filter(releasedate == "5/8" | releasedate == "5/12") %>% 
+  filter(releasedate == "5/12" | releasedate == "5/15") %>% 
   count(facility)
 
 # LOOP THAT DOESNT QUITE WORK/i need to figure out
-test <- alloutbreaks
-for(string in may8outbreaks$facility) if (string %in% "Center") test$facility <- "bob"
+# test <- alloutbreaks
+# for(string in may8outbreaks$facility) if (string %in% "Center") test$facility <- "bob"
